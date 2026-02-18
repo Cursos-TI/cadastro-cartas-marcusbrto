aa7f707cf3afc035d75f3385ded21d2d36016f0d
Marcus Vinicius <35403809+marcusbrto@users.noreply.github.com>
2026-02-17 22:20:23 -0300

    Desafio Nível Mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,20 +2,30 @@
 
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das cartas
-// Nível aventureiro
+// Nível mestre
 
 int main() {
     // Área para definição das variáveis para armazenar as propriedades das cidades
 
     // Primeira carta
     char estado01[20], nome_cidade01[50];
-    int populacao01, num_ponto_turistico01, codigo01;
-    float area01, pib01, densidade01, pib_per_capita01;
+    unsigned int populacao01;
+    double pib01;
+    int num_ponto_turistico01, codigo01;
+    float area01, densidade01, pib_per_capita01;
 
     // Segunda carta
     char estado02[20], nome_cidade02[50];
-    int populacao02, num_ponto_turistico02, codigo02;
-    float area02, pib02, densidade02, pib_per_capita02;
+    unsigned int populacao02;
+    double pib02;
+    int num_ponto_turistico02, codigo02;
+    float area02, densidade02, pib_per_capita02;
+
+    //Resultados
+    int resultado_populacao,resultado_area,resultado_pib,resultado_pontos,resultado_densidade,resultado_pib_per_capita;
+
+    float superpoder01, superpoder02;
+    int resultado_super_poder;
 
     // Área para entrada de dados
 
@@ -72,6 +82,9 @@ int main() {
     pib_per_capita01 = pib01 / populacao01;
     pib_per_capita02 = pib02 / populacao02;
 
+    superpoder01 = populacao01 + area01 + pib01 + num_ponto_turistico01 + densidade01 + pib_per_capita01;
+    superpoder02 = populacao02 + area02 + pib02 + num_ponto_turistico02 + densidade02 + pib_per_capita02;
+
     // Área para exibição dos dados da cidade
 
     printf("\n==== PRIMEIRA CARTA ====\n");
@@ -96,6 +109,32 @@ int main() {
     printf("Pontos turisticos: %d\n", num_ponto_turistico02);
     printf("Densidade populacional: %f\n",densidade02);
     printf("PIB per capita: %f\n",pib_per_capita02);
+
+    //Comparando as cartas
+    resultado_populacao = populacao01 > populacao02;
+    resultado_area = area01 > area02;
+    resultado_pib = pib01 > pib02;
+    resultado_pontos = num_ponto_turistico01 > num_ponto_turistico02;
+    /* densidade é o contrário: menor vence */
+    resultado_densidade = densidade01 < densidade02;
+    resultado_pib_per_capita = pib_per_capita01 > pib_per_capita02;
+    resultado_super_poder = superpoder01 > superpoder02;
+
+    printf("\n==== RESULTADO DAS COMPARACOES ====\n");
+
+    printf("Populacao (Carta 1 venceu?): %d\n", resultado_populacao);
+    printf("Area (Carta 1 venceu?): %d\n", resultado_area);
+    printf("PIB (Carta 1 venceu?): %d\n", resultado_pib);
+    printf("Pontos Turisticos (Carta 1 venceu?): %d\n", resultado_pontos);
+    printf("Densidade Populacional (Carta 1 venceu?): %d\n", resultado_densidade);
+    printf("PIB per capita (Carta 1 venceu?): %d\n", resultado_pib_per_capita);
+
+    printf("\nSuper Poder Carta 1: %f\n", superpoder01);
+    printf("Super Poder Carta 2: %f\n", superpoder02);
+
+    printf("Super Poder (Carta 1 venceu?): %d\n", resultado_super_poder);
+
+
 
     return 0;
 }
